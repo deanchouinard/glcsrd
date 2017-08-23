91f3d4fd5f6cafa5a0d9d9d05f3ae76b72bc2ea4
Dean Chouinard <deanchouinard@gmail.com>
2017-08-23 09:42:20 -0400

    added Chart.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,68 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import Chart from 'chart.js';
+var ctx = document.getElementById("myChart");
+  // var Chart = require("chart.js");
+//import Chart from 'chart.js';
+var ctxLine = document.getElementById("myLineCxt");
+if (ctxLine) {
+    var myLineChart = new Chart(ctxLine, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: [10, 20, 30, 40, 50, 60]
+        }],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    min: 'January'
+                }
+            }]
+        }
+    }
+  });
+};
+
+if (ctx) {
+  var myCart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+  });
+};
+
